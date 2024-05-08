6b8b0f591a91152104f3dd2581ad4e1cd8dc87dc
Boris Nacev <bnachev77@gmail.com>
2024-05-08 03:41:21 +0200

    added functionality to nav and added placeholders for sections

--- scrypt.js
+++ scrypt.js
@@ -1,20 +1,39 @@
 const nav = document.getElementsByClassName("nav-li");
 
 Array.from(nav).forEach((li) => {
-  li.addEventListener("mouseover", function () {
-    li.style.width = "150px";
-    li.style.color = "white";
+  li.addEventListener("mouseover", () => {
+    if (!li.classList.contains("active")) {
+      li.classList.add("active");
+    }
+  });
 
-    li.addEventListener("mouseout", function () {
-      li.style.width = "15px";
-      li.style.color = "transparent";
-    });
+  li.addEventListener("mouseout", () => {
+    if (!li.classList.contains("clicked")) {
+      li.classList.remove("active");
+    }
+  });
 
-    li.addEventListener("click", function (event) {
-      for (const li of document.querySelectorAll("li.active")) {
-        li.classList.remove("active");
-      }
-      event.currentTarget.classList.add("active");
-    });
+  li.addEventListener("click", (event) => {
+    const clickedLi = event.currentTarget;
+    const clickedId = clickedLi.id;
+
+    for (const li of document.querySelectorAll("li.active")) {
+      li.classList.remove("active");
+      li.classList.remove("clicked");
+    }
+
+    clickedLi.classList.add("active");
+    clickedLi.classList.add("clicked");
+
+    for (const section of document.querySelectorAll("section.active-section")) {
+      section.classList.remove("active-section");
+    }
+
+    const currentSection = document.querySelector(`section#${clickedId}`);
+    if (currentSection) {
+      currentSection.classList.add("active-section");
+    } else {
+      console.error(`Section with ID '${clickedId}' not found.`);
+    }
   });
 });
